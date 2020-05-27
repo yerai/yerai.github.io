f60bdaa42eb046d09cc73f8a94f60b280ac36a20
Yerai <yeerizg@gmail.com>
2020-05-27 02:28:27 +0200

    Bigger Index

---
+++ js/project-script.js
@@ -0,0 +1,16 @@
+$(window).on("load", function() {
+    var elems = document.body.getElementsByTagName("picture");
+    for(var i=0; i<elems.length; i++){
+    	elems[i].style.display="block";
+    }
+
+    var elems = document.body.getElementsByTagName("div");
+    for(var i=0; i<elems.length; i++){
+    	elems[i].style.display="block";
+    }
+
+    var elems = document.body.getElementsByTagName("a");
+    for(var i=0; i<elems.length; i++){
+    	elems[i].style.display="block";
+    }
+});
