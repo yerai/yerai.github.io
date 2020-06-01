818721bb1d788696666718bfe8fa6d21638fafff
Yerai <yeerizg@gmail.com>
2020-06-01 08:08:37 +0200

    Resume + flowlight

--- js/script.js
+++ js/script.js
@@ -2,3 +2,26 @@
 $('#project-list').mousewheel(function(e, delta) {
 	this.scrollLeft -= (delta);
 });
+
+//Email
+function Clipboard_CopyTo(value) {
+  var tempInput = document.createElement("input");
+  tempInput.value = value;
+  document.body.appendChild(tempInput);
+  tempInput.select();
+  document.execCommand("copy");
+  document.body.removeChild(tempInput);
+}
+
+function copyEmail() {
+   Clipboard_CopyTo('yeerizg@gmail.com');
+  
+  var tooltip = document.getElementById("email");
+  tooltip.innerHTML = "COPIED!";
+}
+
+function outFunc() {
+  var tooltip = document.getElementById("email");
+  tooltip.innerHTML = "";
+}
+
